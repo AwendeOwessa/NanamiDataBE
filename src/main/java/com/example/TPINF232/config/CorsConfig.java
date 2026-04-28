57c101274940383adfe5708899466af315b18e09
joseph <josephkaisen66@gmail.com>
2026-04-28 14:14:14 +0100

    CORS mis à jour avec url vercel finale

--- src/main/java/com/example/TPINF232/config/CorsConfig.java
+++ src/main/java/com/example/TPINF232/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
                         .allowedOrigins(
                                 "http://127.0.0.1:5500",
                                 "http://localhost:5500",
-                                "https://a-remplacer-apres.vercel.app"
+                                "https://nanami-data-fe.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")

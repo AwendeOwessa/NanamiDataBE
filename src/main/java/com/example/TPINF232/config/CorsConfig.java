0754ee3bb3e0b3bb17c5002380e9e57622157a34
joseph <josephkaisen66@gmail.com>
2026-04-28 14:03:53 +0100

    CORS mis à jour avec url vercel

--- src/main/java/com/example/TPINF232/config/CorsConfig.java
+++ src/main/java/com/example/TPINF232/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://127.0.0.1:5500",
-                                "http://localhost:5500"
+                                "http://localhost:5500",
+                                "https://a-remplacer-apres.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
